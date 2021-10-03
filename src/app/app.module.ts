2ed61e879747997aaa63cffffc98ec6c1657f969
Kishan_Malinda <kishanmalinda207@gmail.com>
2021-10-04 00:44:54 +0530

    Nav Bar Completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { New1Component } from './components/new1/new1.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    New1Component
+    New1Component,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
